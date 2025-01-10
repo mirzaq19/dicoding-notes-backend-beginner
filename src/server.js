df01bff36b4620a9c817a6dfe3d04c0cc4f0ca33
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2025-01-10 22:32:05 +0700

    chore: no need to disable eslint undef rule manually in file

--- src/server.js
+++ src/server.js
@@ -4,13 +4,12 @@ import routes from './routes.js';
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    // eslint-disable-next-line no-undef
-    host: process.env.NODE_ENV === 'production'? '0.0.0.0' : 'localhost',
+    host: process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost',
     routes: {
       cors: {
         origin: ['http://notesapp-v1.dicodingacademy.com'],
-      }
-    }
+      },
+    },
   });
 
   server.route(routes);
